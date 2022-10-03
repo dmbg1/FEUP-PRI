ad365aef4221c56a7187c4d4c16376390db30a53
Tiago Rodrigues <tiaguito.andre21764@gmail.com>
2022-10-03 21:31:53 +0100

    Python script to clear-2

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,5 +1,19 @@
-import pandas
+import pandas as pd
+import numpy as np
 
-data = pandas.read_csv("../data/fake.csv")
 
-print(data.head())
+data = pd.read_csv("../data/fake.csv")
+
+data.drop(inplace=True, columns=['uuid', 'ord_in_thread','crawled','main_img_url', 'domain_rank','replies_count','participants_count','likes', 'comments', 'shares', 'thread_title'])
+data['author'].fillna('Anonymous',inplace=True)
+data = data[data['text'].notna()]              
+data = data[data['title'].notna()]              # 5% Na Values
+data = data[data['country'].notna()]            # 1% Na Values
+
+data.drop_duplicates(inplace=True)
+
+data.to_csv('../data/fakeNoNaNoDup.csv',sep=';', index=False)
+
+
+print("Total:",len(data.index))
+print(len(data['text'].unique()))
