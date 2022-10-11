cb0b1ca5ccbf7e14193438eb768b4cc2bdc595ec
dmbg1 <dmbg1999@hotmail.com>
2022-10-11 23:43:07 +0100

    Added null amounts by column (in %) bar chart

--- src/data_analysis/data_quality.py
+++ src/data_analysis/data_quality.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("./data/fake.csv")
 
 def generate_frequency_pie_chart(column_name, pie_chart_title, gen_file_name):
     def my_autopct(pct):
@@ -15,6 +14,36 @@ def generate_frequency_pie_chart(column_name, pie_chart_title, gen_file_name):
     plt.savefig("./data/plots/" + gen_file_name + ".png")
     plt.close()
 
+
+def generate_null_bar_chart(): 
+    X = ['title', 'country', 'body', 'author']
+
+    title_col_non_nulls = round(df['title'].count() * 100 / len(df), 2)
+    country_col_non_nulls = round(df['country'].count() * 100 / len(df), 2)
+    body_col_non_nulls = round(df['text'].count() * 100 / len(df), 2)
+    author_col_non_nulls = round(df['author'].count() * 100 / len(df), 2)
+
+    non_null_percts = [title_col_non_nulls, country_col_non_nulls, body_col_non_nulls, author_col_non_nulls]
+    null_percts = [100 - title_col_non_nulls, 100 - country_col_non_nulls, 100 - body_col_non_nulls, 100 - author_col_non_nulls]
+
+    X_axis = np.arange(len(X))
+
+    bar1 = plt.bar(X_axis - 0.2, non_null_percts, 0.4, label = 'Non-Nulls %')
+    bar2 = plt.bar(X_axis + 0.2, null_percts, 0.4, label = 'Nulls %')
+
+    plt.bar_label(bar1, fmt='%.1f%%', label_type='edge')
+    plt.bar_label(bar2, fmt='%.1f%%', label_type='edge')
+
+    plt.xticks(X_axis, X)
+    plt.xlabel("Columns")
+    plt.ylabel("Percentage of Nulls and Non-Nulls")
+    plt.title("Amount of Nulls/Non-Nulls in the different columns")
+    plt.legend()
+    plt.savefig('./data/plots/nulls_plot.png')
+    plt.close()
+
+df = pd.read_csv("./data/fake.csv")
+
 # Dropped Columns with Frequencies
 generate_frequency_pie_chart("replies_count", "Reply Frequency", "replies_count_pie_chart")
 generate_frequency_pie_chart("participants_count", "Comment Frequency", "participants_count_pie_chart")
@@ -23,8 +52,4 @@ generate_frequency_pie_chart("comments", "Comment Frequency", "comments_pie_char
 generate_frequency_pie_chart("shares", "Shares Frequency", "shares_pie_chart")
 
 # Dropped NULLs in columns quantity
-#data = data[data['body'].notna()]              
-#data = data[data['title'].notna()]              # 5% Na Values
-#data = data[data['country'].notna()]            # 1% Na Values
-# Columns that don't make sense keeping
-# Duplicates
+generate_null_bar_chart()
