93dc1fd16f21b2d5246b456999537d7c664166f0
Diogo Faria <dluis34@gmail.com>
2022-11-11 12:56:39 +0000

    a few changes

--- M2/get_queries.py
+++ M2/get_queries.py
@@ -5,9 +5,11 @@ QUERY_URL = "http://localhost:8983/solr/news/select"
 
 # Does Alex (author) write news about Russia?
 params = {
-    'q': 'author:Alex (title:Russia OR body_en:Russia OR body_fr:Russie OR body_de:Russland OR body_es:Rusia OR body_ru:Россия)',
+    'q': 'author:Alex (title:Russia OR body_en:Russia)',
     'indent': 'true',
     'q.op': 'AND',
+    'defType': 'edismax',
+    'qf': 'title^2',
     'wt': 'json',
     'rows': 10
 }
@@ -16,9 +18,8 @@ results = requests.get(QUERY_URL, params=params).json()['response']['docs']
 
 # Conspiracy fake news about the FBI
 params = {
-    'q': '((title:FBI OR body_fr:FBI OR body_en:FBI OR body_de:FBI OR body_es:FBI OR body_ru:FBI) AND type:conspiracy)^=2 ' +
-            '((title:FBI OR body_fr:FBI OR body_en:FBI OR body_de:FBI OR body_es:FBI OR body_ru:FBI) AND ' +
-            '(body_fr:conspiration OR body_en:conspiracy OR body_de:verschwörung OR body_es:conspiración OR body_ru:конспирология))^=1',
+    'q': '((title:FBI OR body_en:FBI) AND type:conspiracy)^=2' +
+            '((title:FBI OR body_en:FBI) AND (title:conspiracy OR body_en:conspiracy))^=1',
     'indent': 'true',
     'q.op': 'OR',
     'wt': 'json',
@@ -29,23 +30,24 @@ results = requests.get(QUERY_URL, params=params).json()['response']['docs']
 
 # What highly spammy news have there been about war?
 params = {
-    'q': 'title:war body_fr:guerre body_en:war body_de:krieg body_es:guerra body_ru:война',
+    'q': 'title:war body_en:war',
     'indent': 'true',
     'q.op': 'OR',
     'defType': 'edismax',
     'qf': 'title^2',
-    'sort': 'spam_score desc',
+    'fq': 'spam_score:[0.8 TO *]',
     'wt': 'json',
-    'rows': 10
+    'rows': 1000
 }
 
 results = requests.get(QUERY_URL, params=params).json()['response']['docs']
+print(len(results))
 
 # Fake news related to the presidential elections in the United States
 # NEEDS TO BE FINISHED
 params = {
-    'q': 'country:US AND (title:"elections" OR body_fr:"élections" OR body_en:"elections" OR body_de:"wahlen" OR body_es:"elecciones" OR body_ru:"выборы" )' + 
-            '(title:"elections" OR body_fr:"élections" OR body_en:"elections" OR body_de:"wahlen" OR body_es:"elecciones" OR body_ru:"выборы" )',
+    'q': 'country:US AND (title:"elections" OR body_en:"elections")' + 
+            '(title:"elections" OR body_en:"elections")',
     'indent': 'true',
     'q.op': 'OR',
     'wt': 'json',
@@ -57,9 +59,9 @@ results = requests.get(QUERY_URL, params=params).json()['response']['docs']
 # Fake news in Colombia about the US
 params = {
     'q': 'country:CO ' + 
-            '((title:"United States" OR body_fr:"États Unis" OR body_en:"United States" OR body_de:"Vereinigte Staaten" OR body_es:"Estados Unidos" OR body_ru:"Соединенные Штаты") OR' + 
-            '((title:"USA" OR body_fr:"USA" OR body_en:"USA" OR body_de:"USA" OR body_es:"USA" OR body_ru:"США")) OR' +
-            '((title:"US" OR body_fr:"US" OR body_en:"US" OR body_de:"US" OR body_es:"US" OR body_ru:"США")^=0.5))',
+            '((title:"United States" OR body_en:"United States") OR' + 
+            '((title:"USA" OR body_en:"USA") OR' +
+            '((title:"US" OR body_en:"US")^=0.5))',
     'indent': 'true',
     'q.op': 'AND',
     'wt': 'json',
