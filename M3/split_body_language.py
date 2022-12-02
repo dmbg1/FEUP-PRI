eab1669581262a807aef54b0dac42d5bb2f76db7
Diogo Faria <dluis34@gmail.com>
2022-12-02 11:14:46 +0000

    split titles into different languages

--- M3/split_body_language.py
+++ M3/split_body_language.py
@@ -3,20 +3,34 @@ import pandas as pd
 
 data = pd.read_csv("./data/fake_clean.csv")
 
-#print(data['language'].unique())
+data.drop_duplicates(subset=['title', 'site_url'], inplace=True)
 
 langsToRemove = []
 
-for lang in data['language'].unique():
+""" for lang in data['language'].unique():
     print(lang, len(data[data['language'] == lang]))
     if(len(data[data['language'] == lang]) < 25):
-        langsToRemove.append(lang)
+        langsToRemove.append(lang) """
 
-print(langsToRemove)
+#print(langsToRemove)
 
 data = data[~data['language'].isin(langsToRemove)]
 
-print(data['language'].unique())
+#print(data['language'].unique())
+
+data['title_en'] = None
+data['title_de'] = None
+data['title_fr'] = None
+data['title_es'] = None
+data['title_ru'] = None
+
+data.loc[data['language'] == 'english', 'title_en'] = data['title']
+data.loc[data['language'] == 'german', 'title_de'] = data['title']
+data.loc[data['language'] == 'french', 'title_fr'] = data['title']
+data.loc[data['language'] == 'spanish', 'title_es'] = data['title']
+data.loc[data['language'] == 'russian', 'title_ru'] = data['title']
+
+data.drop(columns=['title'], inplace=True)
 
 data['body_en'] = None
 data['body_de'] = None
@@ -34,6 +48,4 @@ data.drop(columns=['body'], inplace=True)
 
 data['id'] = data.index
 
-data = data[data['id'] != 2660]
-
-data.to_csv('./data/fake_clean_body_split.csv', index=False)
+data.to_csv('./data/fake_clean_lang_split.csv', index=False)
